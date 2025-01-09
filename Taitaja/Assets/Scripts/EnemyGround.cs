d38c60e4c9465352c5e68b9ce2620652b3c0518c
SamuPaju <151126865+SamuPaju@users.noreply.github.com>
2025-01-09 13:19:54 +0200

    Ground Enemy
    Ground enemy finished.
    IMPORTANT:
    You'll need to make own animations for every enemy + you'll need to make the animator same as the EnemyRocks.

--- Taitaja/Assets/Scripts/EnemyGround.cs
+++ Taitaja/Assets/Scripts/EnemyGround.cs
@@ -7,19 +7,43 @@ public class EnemyGround : MonoBehaviour
     public float speed = 1; // Walking speed
 
     Rigidbody2D rb;
+    Animator anim;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     void Update()
     {
+        // Move enemy
         rb.velocity = Vector2.right * speed;
+
+        /* IMPORTANT!
+         * You'll need to make own animations for every enemy + 
+         * you'll need to make the animator same as the EnemyRocks.
+         */
+        // Set animations based on speed
+        if (speed == 0)
+        {
+            anim.SetBool("IsRun", false);
+        }
+        else if (speed > 0)
+        {
+            anim.SetBool("IsRun", true);
+            GetComponent<SpriteRenderer>().flipX = true;            
+        }
+        else if (speed < 0)
+        {
+            anim.SetBool("IsRun", true);
+            GetComponent<SpriteRenderer>().flipX = false;
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        // Change the direction
         if (collision.CompareTag("EndOfLine"))
         {
             speed *= -1;
