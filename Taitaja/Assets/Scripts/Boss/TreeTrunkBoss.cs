f83d402fd7f79575375e9b99ce668ce62e9c4248
SamuPaju <151126865+SamuPaju@users.noreply.github.com>
2025-01-12 16:58:18 +0200

    Boss finished
    Boss is finished for now.

--- Taitaja/Assets/Scripts/Boss/TreeTrunkBoss.cs
+++ Taitaja/Assets/Scripts/Boss/TreeTrunkBoss.cs
@@ -6,14 +6,14 @@ public class TreeTrunkBoss : MonoBehaviour
 {
     [Header("Map variables")]
     public GameObject leftWall;
-
+    // Obsticle set 1
     [SerializeField] GameObject obsticles1;
     [SerializeField] GameObject obsCol1;
-
+    // Obsticle set 2
     [SerializeField] GameObject obsticles2;
     [SerializeField] GameObject obsCol2;
 
-    [Header("Component")]
+    [Header("Components")]
     Animator anim;
 
     [Header("Value variables")]
@@ -21,12 +21,10 @@ public class TreeTrunkBoss : MonoBehaviour
     float timeCounter;
     int health = 3;
     public bool isHit = false;
-    //public bool isAttacking = false;
 
+    [Header("Bullet realted variables")]
     [SerializeField] GameObject bulletPrefab;
     [SerializeField] GameObject firingSpot;
-
-
 
     private void Awake()
     {
@@ -36,6 +34,7 @@ public class TreeTrunkBoss : MonoBehaviour
     void Start()
     {
         timeCounter = 0;
+        leftWall.SetActive(true);
     }
 
     void Update()
@@ -80,6 +79,39 @@ public class TreeTrunkBoss : MonoBehaviour
     {
         isHit = true;
         health--;
+        StartCoroutine(NextStage());
         anim.SetTrigger("Hit");
+    }
+
+    /// <summary>
+    /// After taking damage start the next stage and make attackSpeed lower
+    /// </summary>
+    /// <returns></returns>
+    IEnumerator NextStage()
+    {
+        if (health == 2)
+        {
+            attackSpeed *= 0.7f;
+            yield return new WaitForSeconds(4);
+            obsticles1.SetActive(true);
+            obsCol1.SetActive(true);
+        }
+        else if (health == 1)
+        {
+            attackSpeed *= 0.5f;
+            obsticles1.SetActive(false);
+            obsCol1.SetActive(false);
+            yield return new WaitForSeconds(6);
+            obsticles2.SetActive(true);
+            obsCol2.SetActive(true);
+        }
+        else if (health == 0)
+        {
+            attackSpeed *= 100;
+            obsticles2.SetActive(false);
+            obsCol2.SetActive(false);
+            yield return new WaitForSeconds(2f);
+            gameObject.SetActive(false);
+        }
     }
 }
